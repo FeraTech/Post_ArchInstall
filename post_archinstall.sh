a8bd886deb4c69ef3a1736645f8f6d893b72b207
FeraTech <feratech0@gmail.com>
2023-07-06 18:58:38 -0600

    DE-Hop to gnome (again)

--- post_archinstall.sh
+++ post_archinstall.sh
@@ -28,20 +28,21 @@ sleep 3s
 yay -S ttf-dejavu noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-bitstream-vera ttf-liberation ttf-roboto ttf-roboto-mono ttf-ubuntu-font-family ttf-jetbrains-mono ttf-inconsolata otf-crimson ttf-carlito ttf-ms-fonts otf-takao
 echo "Installing gtk stuff"
 sleep 3s
-yay -S gtk-engine-murrine gtk-engines arc-gtk-theme papirus-icon-theme materia-gtk-theme
+## yay -S gtk-engine-murrine gtk-engines arc-gtk-theme papirus-icon-theme materia-gtk-theme
+yay -S gnome-themes-extra papirus-icon-theme
 echo "Installing other tools"
 sleep 3s
-yay -S neofetch htop btop figlet cmatrix tree asciiquarium unrar zip unzip p7zip neovim xsel wget man perl-rename ufw
-## sudo ufw enable
+yay -S neofetch htop btop figlet cmatrix tree asciiquarium unrar zip unzip p7zip neovim xsel wget man perl-rename gufw
+sudo ufw enable
 echo "Installing AUR tools"
 sleep 3s
-yay -S pfetch inxi aic94xx-firmware wd719x-firmware upd72020x-fw libva-utils
+yay -S pfetch inxi libva-utils
 echo "Installing multimedia stuff"
 sleep 3s
 yay -S vlc gstreamer gstreamer-vaapi gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly intel-gpu-tools
 echo "Installing pipewire and audio stuff"
 sleep 3s
-yay -S pipewire gst-plugin-pipewire pipewire-v4l2 pipewire-x11-bell pipewire-zeroconf realtime-privileges helvum
+yay -S pipewire gst-plugin-pipewire pipewire-v4l2 pipewire-x11-bell pipewire-zeroconf realtime-privileges
 echo "Adding user to the realtime group"
 sleep 3s
 sudo usermod -a -G realtime $USER
@@ -53,14 +54,16 @@ sleep 3s
 yay -S gparted jfsutils f2fs-tools exfatprogs reiserfsprogs udftools nilfs-utils gpart gnome-disk-utility
 echo "Installing more apps"
 sleep 3s
-yay -S chezmoi transmission-qt obs-studio audacity pdfarranger bleachbit gimp inkscape minder shotcut clipgrab handbrake gummi netbeans zoom
+## yay -S chezmoi transmission-qt obs-studio audacity pdfarranger bleachbit gimp inkscape minder shotcut clipgrab handbrake gummi netbeans zoom
+yay -S chezmoi fragments obs-studio audacity bleachbit gimp inkscape minder shotcut clipgrab handbrake netbeans zoom
 echo "Installing KDE apps"
 sleep 3s
-yay -S spectacle gwenview okular packagekit-qt5 kdenlive krita elisa kdeconnect arc-kde materia-kde
+## yay -S spectacle gwenview okular packagekit-qt5 kdenlive krita elisa kdeconnect arc-kde materia-kde
 echo "Installing even more apps"
 sleep 3s
-yay -S code retroarch retroarch-assets-glui retroarch-assets-ozone retroarch-assets-xmb libretro-core-info libretro-overlays libretro-shaders-slang spotify ardour bitwarden
-echo "Installing some themes"
+yay -S code retroarch retroarch-assets-glui retroarch-assets-ozone retroarch-assets-xmb libretro-core-info libretro-overlays libretro-shaders-slang spotify ardour bitwarden joplin-desktop
+echo "Installing some apps"
 sleep 3s
-yay -S qogir-gtk-theme qogir-kde-theme-git qogir-icon-theme tela-icon-theme
+yay -S stremio
+## yay -S qogir-gtk-theme qogir-kde-theme-git qogir-icon-theme tela-icon-theme
 echo "OK! :))"
